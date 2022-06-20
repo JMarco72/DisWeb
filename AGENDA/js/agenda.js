293df430bc13eb52ffa8c6c37a6a73e95532c0dd
Rodas <jleonarrv@gmail.com>
2022-06-19 21:03:12 -0500

    trabajos

---
+++ AGENDA/js/agenda.js
@@ -0,0 +1,46 @@
+function guardarDatos() {
+    var nombre = document.getElementById("nombre").value;
+    var movil = document.getElementById("movil").value;
+    localStorage.setItem(nombre,movil);
+    document.getElementById("nombre").value = "";
+    document.getElementById("movil").value = "";
+    actualizarDatos();
+}
+
+function recuperarDatos() {
+    var nombre = document.getElementById("nombre").value;
+    localStorage.getItem(nombre);
+    document.getElementById("movil").value = localStorage.getItem(nombre);
+}
+
+function eliminarDatos() {
+    var nombre = document.getElementById("nombre").value;
+    localStorage.getItem(nombre);
+    actualizarDatos();
+}
+
+function aliminarTodo() {
+    localStorage.clear();
+    actualizarDatos();
+}
+
+function actualizarDatos(){
+    var registro = "";
+    if (localStorage.length == 0) {
+        registro += '<li>Vacio</li>';
+    } else {
+        for(var i = 0;  i <=localStorage.length -  1; i++){
+            var key = localStorage.key(i);
+            registro += '<li>' + '<span class="nom">' + key + '</span>'
+            + '<span class="nom">' + localStorage.getItem(key) + '</span>' + '</li><br>';
+        }
+    }
+    document.getElementById('contactos').innerHTML = registro;
+}
+
+//validar solo 9 numeros en el telefono
+var input=  document.getElementById('movil');
+input.addEventListener('input',function(){
+  if (this.value.length > 9) 
+     this.value = this.value.slice(0,9); 
+})
